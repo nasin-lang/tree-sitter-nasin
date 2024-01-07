c92bd8f396e4ccafd30fd3907dcf07a097296411
FelipeSharkao <felipenasgomes@gmail.com>
2024-01-06 21:12:59 -0300

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-type-checked"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    root: true,
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: true,
+    },
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Torvo Language
+
+Torvo is a pure functional language with a easy syntax and a strong expressive type system.
+
+## Warning
+
+This project is far from complete and not at all ready for production. Use with caution.
